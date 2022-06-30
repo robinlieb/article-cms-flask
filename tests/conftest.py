cb91e4412e62f9b2022e80271fbb100eb7ea01c9
Robin Lieb <robin.j.lieb@gmail.com>
2022-07-01 00:13:05 +0200

    test: added test config and login test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,32 @@
 import flask
-from FlaskWebProject.models import User
-from FlaskWebProject import create_app
+from FlaskWebProject.models import User, Post
+from FlaskWebProject import create_app, db
+from instance.config import TestConfig
 import pytest
 
 @pytest.fixture(scope='module')
 def test_client():
-    flask_app = create_app()
-    flask_app.testing = True
+    flask_app = create_app(TestConfig)
 
     with flask_app.test_client() as test_client:
-        yield test_client
+        with flask_app.app_context():
+            yield test_client
+
+@pytest.fixture(scope='module')
+def init_database(test_client):
+
+    db.create_all()
+
+    user = User()
+    user.username = "admin"
+    user.password_hash = 'pbkdf2:sha256:260000$p1hn7qX6mzriQDYs$5859d3d329d33d7c88c9f568aec82cef54c2cbdfa04674994cb3f91d1ed036e2'
+    db.session.add(user)
+
+    db.session.commit()
+
+    yield
+
+    db.drop_all()
 
 @pytest.fixture(scope='module')
 def new_user():
--- tests/functional/test_login.py
+++ tests/functional/test_login.py
@@ -6,6 +6,23 @@ def test_login_page_with_anonymous_user(test_client):
     THEN check that the response is valid
     """
 
-    response = test_client.get('/')
-    assert response.status_code == 302
-    assert b'login' in response.data
+    response = test_client.get('/', follow_redirects=True)
+    assert response.status_code == 200
+    print(response.data)
+    assert b'Sign In' in response.data
+
+def test_login(test_client, init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is requested (POST) with known user
+    THEN check that the response is valid
+    """
+
+    response = test_client.post(
+        '/login',
+        data=dict(username='admin', password='helloworld'),
+        follow_redirects=True
+    )
+
+    assert response.status_code == 200
+    assert b'Logout' in response.data

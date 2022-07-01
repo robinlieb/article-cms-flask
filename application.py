07fa23d203befca141cd8b5c45ebd4829f99a6be
Robin Lieb <robin.j.lieb@gmail.com>
2022-07-01 07:59:36 +0200

    fix: issue with gunicorn finding app

--- application.py
+++ application.py
@@ -5,11 +5,12 @@ This script runs the FlaskWebProject application using a development server.
 from os import environ
 from FlaskWebProject import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app = create_app()
     app.run(HOST, PORT, ssl_context='adhoc')
